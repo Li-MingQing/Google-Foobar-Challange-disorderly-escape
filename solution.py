ba1e049592187a20a42e34bdbfc2264be37ea6df
Li-MingQing <mingqing.ca@gmail.com>
2023-01-28 17:28:04 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+"""
 Disorderly Escape
 =================
 Oh no! You've managed to free the bunny prisoners and escape Commander Lambdas exploding space station, but her team of elite starfighters has flanked your ship. If you dont jump to hyperspace, and fast, youll be shot out of the sky!
@@ -45,3 +46,37 @@ Inputs:
     (int) s = 4
 Output:
     (string) "430"
+
+"""  
+from math import factorial
+from fractions import gcd
+
+
+def solution(w, h, s):
+    answer = 0
+    for c in integer_partitions(w):
+        for r in integer_partitions(h):
+            m = conjugacy_size(r) * conjugacy_size(c)
+            answer += m * pow(s, sum([sum([gcd(i, j) for i in r]) for j in c]))
+    answer = answer / (factorial(w) * factorial(h))
+    return str(answer)
+
+def integer_partitions(n, i =1):
+    yield [n]
+    for i in range(i, n // 2 + 1):
+        for p in integer_partitions(n - i, i):
+            yield [i] + p
+
+def conjugacy_size(nums):
+    ans = {}
+    s = sum(nums)
+    for item in nums:
+        if item not in ans:
+            n = nums.count(item)
+            ans.update({item:n})
+        else:
+            continue
+    res = factorial(s)
+    for num, count in ans.items():
+        res = res/(pow(num, count)*factorial(count))
+    return res
